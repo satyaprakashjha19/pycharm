d3eed2889c31ca9b061851dcde34e2108bd81f50
Satyaprakash jha <satyaprakashjha19@yahoo.com>
2023-01-23 19:28:29 +0530

    numpy one dimensional

--- numpractice.py
+++ numpractice.py
@@ -20,3 +20,90 @@ stu_roll2 = numpy.array([101, 102, 103, 104, 105])
 n = len(stu_roll2)
 for i in range(n):
     print(i, "=",  stu_roll2[i])
+
+# Zeros function default float value
+from numpy import *
+a = zeros(5)
+n = len(a)
+for i in range(n):
+    print('index', i, '=', a[i])
+
+# ones function default float value
+from numpy import *
+one = ones(5)
+n = len(one)
+for i in range(n):
+    print(one[i])
+
+# arange function
+
+from numpy import *
+a = arange(5, 20, 2)
+n = len(a)
+for i in range(n):
+    print("index", i, "=", a[i])
+
+
+from numpy import *
+k = array([101, 102, 103, 104, 105])
+A = k -5
+for i in A:
+    print(i)
+
+from numpy import *
+k = array([101, 102, 103, 104, 105])
+l = array([10, 200, 15, 120, 25])
+result = where(k>l, k, l)
+print(result)
+
+"""Getting input in numpy one dimensional array
+from user for loop"""
+
+from numpy import *
+n = int(input("Enter no. of elements: "))
+a = zeros(n, dtype=int)
+n = len(a)
+print((a))
+for i in range(n):
+    x = int(input("Enter elements: "))
+    a[i] = x
+
+print(a)
+
+for i in range(n):
+    print(i, "=", a[i])
+
+
+"""Getting input in numpy one dimensional array 
+from user using while loop"""
+
+from numpy import *
+n = int(input("Enter no. of elements: "))
+z = zeros(n, dtype=int)
+n = len(z)
+
+print(z)
+
+i = 0
+j = 0
+while i < n:
+    x = int(input("Enter elements: "))
+    z[i] = x
+    i += 1
+
+print(z)
+
+while j < len(z):
+    print(z[j])
+    j += 1
+
+
+
+
+
+
+
+
+
+
+
